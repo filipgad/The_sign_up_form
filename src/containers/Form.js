d95af96cc9da6b0e8fcfe8130cb7be8d4f2a8bb0
Filip Godziński <godzinski.filip@gmail.com>
2018-02-11 01:10:20 +0100

    destructuring props

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -15,33 +15,8 @@ class Form extends React.Component {
         this.props.formUpdateValue(event.target.name, event.target.value);
     }
 
-    // handleSubmit = () => {
-    //     const eventParticipant = [{
-    //         firstName: this.refs.firstName.value,
-    //         lastName: this.refs.lastName.value,
-    //         email: this.refs.email.value,
-    //         eventData: this.refs.eventData.value
-    //     }]
-    //     this.props.postApplications(eventParticipant);
-    // }
-
-    // handleSubmit = (event) => {
-    //     alert(`A form was submitted: ${this.state.firstName} ${this.state.lastName} ${this.state.email} ${String(this.state.startData)}`);
-    //     axios.post('mongodb://localhost:27017/events', {
-    //         firstName: this.state.firstName,
-    //         lastName: this.state.lastName,
-    //         email: this.state.email,
-    //         eventData: this.state.eventData
-    //     })
-    //         .then(response => {
-    //         console.log(response, 'Event added!');
-    //         })
-    //         .catch(err => {
-    //         console.log(err, 'Event not added, try again');
-    //     });
-    // }
-
     render() {
+        const { firstName, lastName, email, startDate } = this.props.formValues;
         return (
             <form method="post" action="/insert">
                 <h1>SIGN UP FOR THE EVENT</h1>
@@ -49,28 +24,28 @@ class Form extends React.Component {
                     title="First name"
                     type="text" 
                     name="firstName" 
-                    value={this.props.formValues.firstName} 
+                    value={firstName} 
                     onChange={this.handleChange} 
                     placeholder="Enter name" />
                 <FormInput 
                     title="Last name"
                     type="text" 
                     name="lastName" 
-                    value={this.props.formValues.lastName} 
+                    value={lastName} 
                     onChange={this.handleChange} 
                     placeholder="Enter last name" />
                 <FormInput 
                     title="Email"
                     type="email" 
                     name="email" 
-                    value={this.props.formValues.email} 
+                    value={email} 
                     onChange={this.handleChange} 
                     placeholder="Enter email" />
                 <label>Event date</label>
                 <DatePicker
                     name="eventDate"
                     placeholderText="Choose event date"
-                    selected={this.props.formValues.startDate ? moment(this.props.formValues.startDate) : null}
+                    selected={startDate ? moment(startDate) : null}
                     onChange={this.handleDateChange}
                     minDate={moment()}
                     dateFormat="L"
